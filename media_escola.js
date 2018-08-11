c45d2851b33edaaea93f6840dedfda6187047a55
NtkArt <41126564+NtkArt@users.noreply.github.com>
2018-08-11 16:48:46 -0300

    Calculadora Média Escolar
    Calculadora 1.12

--- media_escola.js
+++ media_escola.js
@@ -1,33 +1,42 @@
+function pegaNotas(labelNotas){
+    var notas = prompt(labelNotas);
+    if(notas != null){
+        return parseFloat(notas.replace(',' , '.'));
+    }else{
+        alert('O valor não pode ser vazio.')
+    }
+}
+
 function mediaNotas(){
-    var nota1 = parseFloat(prompt('Digite a nota do 1º bimestre').replace(',','.'))
-    var nota2 = parseFloat(prompt('Digite a nota do 2º bimestre').replace(',','.'))
-    var nota3 = parseFloat(prompt('Digite a nota do 3º bimestre').replace(',','.'))
-    var nota4 = parseFloat(prompt('Digite a nota do 4º bimestre').replace(',','.'))
-    var media = ((nota1 + nota2 + nota3 + nota4) / 4)
-    media = parseFloat(media.toFixed(1))
+    var nota1 = pegaNotas('Digite a nota do 1º bimestre');
+    var nota2 = pegaNotas('Digite a nota do 2º bimestre');
+    var nota3 = pegaNotas('Digite a nota do 3º bimestre');
+    var nota4 = pegaNotas('Digite a nota do 4º bimestre');
+    var media = (nota1 + nota2 + nota3 + nota4) / 4;
+    media = parseFloat(media.toFixed(1));
     
     
     if(media >= 85)
-    alert('Sua média é de: ' + media + '\nAprovado')
+    alert('Sua média é de: ' + media + '\n\nAprovado');
     else{
 
     }
     if(media == 84)
-        alert('Sua média é: ' + media + '\nVocê vai para o conselho de Classe');
+        alert('Sua média é: ' + media + '\n\nVocê vai para o conselho de Classe');
     else{
 
     }
     if(media >= 70 && media <= 84)
-        alert('Sua média é: ' + media + '\nVocê vai para recuperação');
+        alert('Sua média é: ' + media + '\n\nVocê vai para recuperação');
     else{
 
     }
     if(media < 70 && media >=50)
-        alert('Sua média é: ' + media + '\nVocê está REPROVADO!');
+        alert('Sua média é: ' + media + '\n\nVocê está REPROVADO!');
     else{
     }
     if(media < 50)
-        alert('Sua média é: ' + media + '\nVocê nem aqui vai estudar mais, seu burro');
+        alert('Sua média é: ' + media + '\n\nVocê nem aqui vai estudar mais, seu burro');
     else{
         
     }
@@ -36,4 +45,4 @@ function mediaNotas(){
 
 
 
-mediaNotas();
+mediaNotas
